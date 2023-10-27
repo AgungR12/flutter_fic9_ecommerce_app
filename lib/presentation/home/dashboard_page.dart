8de3c19e4a4c889d76e00031b70c31f99d72fb1d
AgungR12 <2000agungramadhan@gmail.com>
2023-10-27 12:31:54 +0700

    flutter app

---
+++ lib/presentation/home/dashboard_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
